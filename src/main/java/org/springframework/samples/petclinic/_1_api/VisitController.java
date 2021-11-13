69894fecfa7df6370e439dadd91cabf295c2e622
Agota <45971426+agosu@users.noreply.github.com>
2021-11-13 22:38:33 +0200

    Updated readme and some other minor changes

--- src/main/java/org/springframework/samples/petclinic/_1_api/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/_1_api/VisitController.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic._1_api;
 
 import org.springframework.samples.petclinic._2_service.VisitService;
-import org.springframework.samples.petclinic._3_repository.PetRepository;
 import org.springframework.samples.petclinic._4_domain.Visit;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -13,7 +12,7 @@ class VisitController {
 
 	private final VisitService visitService;
 
-	public VisitController(VisitService visitService, PetRepository pets) {
+	public VisitController(VisitService visitService) {
 		this.visitService = visitService;
 	}
 
